e64dc5ddd51581f25bfddd80214e4152446b0ad8
PetrikS <fajarnurprasetyo.2@gmail.com>
2023-06-09 12:37:54 +0700

    Initial commit

---
+++ Program Penjualan Barang/Form1.Designer.vb
@@ -0,0 +1,429 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Lbljudul1 = New Label()
+        Lbljudul2 = New Label()
+        PictureBox1 = New PictureBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        PictureBox2 = New PictureBox()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Cbjenis = New ComboBox()
+        Txttotalharga = New TextBox()
+        Txtpajak = New TextBox()
+        Txtdiscount = New TextBox()
+        Txtsubharga = New TextBox()
+        Txtharga = New TextBox()
+        Txtalamat = New TextBox()
+        Txtnama = New TextBox()
+        Txttransaksi = New TextBox()
+        GBkriteria = New GroupBox()
+        RbBebek = New RadioButton()
+        Rbsport = New RadioButton()
+        PictureBox3 = New PictureBox()
+        btBaru = New Button()
+        btKeluar = New Button()
+        Dttanggal = New DateTimePicker()
+        Lbljudul3 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        GBkriteria.SuspendLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Lbljudul1
+        ' 
+        Lbljudul1.AutoSize = True
+        Lbljudul1.Font = New Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point)
+        Lbljudul1.Location = New Point(5, 9)
+        Lbljudul1.Name = "Lbljudul1"
+        Lbljudul1.Size = New Size(283, 45)
+        Lbljudul1.TabIndex = 0
+        Lbljudul1.Text = "PT. Daya Pratama"
+        ' 
+        ' Lbljudul2
+        ' 
+        Lbljudul2.AutoSize = True
+        Lbljudul2.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Lbljudul2.Location = New Point(12, 54)
+        Lbljudul2.Name = "Lbljudul2"
+        Lbljudul2.Size = New Size(237, 15)
+        Lbljudul2.TabIndex = 1
+        Lbljudul2.Text = "Distributor Kendaraan Bermotor Divisi Jawa"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.ButtonShadow
+        PictureBox1.Location = New Point(12, 92)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(560, 2)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(12, 108)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(34, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Kode"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(12, 137)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(93, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Nama Customer"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(12, 195)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(49, 15)
+        Label5.TabIndex = 9
+        Label5.Text = "Tanggal"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(12, 166)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(93, 15)
+        Label6.TabIndex = 7
+        Label6.Text = "Alamat Lengkap"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(381, 166)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(39, 15)
+        Label7.TabIndex = 7
+        Label7.Text = "Harga"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(381, 195)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(55, 15)
+        Label8.TabIndex = 9
+        Label8.Text = "Discount"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.ButtonShadow
+        PictureBox2.Location = New Point(381, 226)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(195, 2)
+        PictureBox2.TabIndex = 2
+        PictureBox2.TabStop = False
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(381, 242)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(52, 15)
+        Label9.TabIndex = 7
+        Label9.Text = "Subtotal"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(381, 271)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(35, 15)
+        Label10.TabIndex = 9
+        Label10.Text = "Pajak"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label11.Location = New Point(381, 300)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(33, 15)
+        Label11.TabIndex = 9
+        Label11.Text = "Total"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Label12.Location = New Point(12, 300)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(92, 15)
+        Label12.TabIndex = 9
+        Label12.Text = "Jenis Kendaraan"
+        ' 
+        ' Cbjenis
+        ' 
+        Cbjenis.FormattingEnabled = True
+        Cbjenis.Location = New Point(111, 297)
+        Cbjenis.Name = "Cbjenis"
+        Cbjenis.Size = New Size(264, 23)
+        Cbjenis.TabIndex = 28
+        ' 
+        ' Txttotalharga
+        ' 
+        Txttotalharga.Location = New Point(442, 297)
+        Txttotalharga.Name = "Txttotalharga"
+        Txttotalharga.ReadOnly = True
+        Txttotalharga.Size = New Size(130, 23)
+        Txttotalharga.TabIndex = 33
+        Txttotalharga.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Txtpajak
+        ' 
+        Txtpajak.Location = New Point(442, 268)
+        Txtpajak.Name = "Txtpajak"
+        Txtpajak.ReadOnly = True
+        Txtpajak.Size = New Size(130, 23)
+        Txtpajak.TabIndex = 32
+        Txtpajak.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Txtdiscount
+        ' 
+        Txtdiscount.Location = New Point(442, 192)
+        Txtdiscount.Name = "Txtdiscount"
+        Txtdiscount.ReadOnly = True
+        Txtdiscount.Size = New Size(130, 23)
+        Txtdiscount.TabIndex = 30
+        Txtdiscount.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Txtsubharga
+        ' 
+        Txtsubharga.Location = New Point(442, 239)
+        Txtsubharga.Name = "Txtsubharga"
+        Txtsubharga.ReadOnly = True
+        Txtsubharga.Size = New Size(130, 23)
+        Txtsubharga.TabIndex = 31
+        Txtsubharga.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Txtharga
+        ' 
+        Txtharga.Location = New Point(442, 163)
+        Txtharga.Name = "Txtharga"
+        Txtharga.ReadOnly = True
+        Txtharga.Size = New Size(130, 23)
+        Txtharga.TabIndex = 29
+        Txtharga.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Txtalamat
+        ' 
+        Txtalamat.Location = New Point(111, 163)
+        Txtalamat.Name = "Txtalamat"
+        Txtalamat.Size = New Size(264, 23)
+        Txtalamat.TabIndex = 24
+        ' 
+        ' Txtnama
+        ' 
+        Txtnama.Location = New Point(111, 134)
+        Txtnama.Name = "Txtnama"
+        Txtnama.Size = New Size(264, 23)
+        Txtnama.TabIndex = 23
+        ' 
+        ' Txttransaksi
+        ' 
+        Txttransaksi.Location = New Point(111, 105)
+        Txttransaksi.Name = "Txttransaksi"
+        Txttransaksi.Size = New Size(264, 23)
+        Txttransaksi.TabIndex = 22
+        ' 
+        ' GBkriteria
+        ' 
+        GBkriteria.Controls.Add(RbBebek)
+        GBkriteria.Controls.Add(Rbsport)
+        GBkriteria.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        GBkriteria.Location = New Point(7, 221)
+        GBkriteria.Name = "GBkriteria"
+        GBkriteria.Size = New Size(242, 70)
+        GBkriteria.TabIndex = 27
+        GBkriteria.TabStop = False
+        GBkriteria.Text = "Kriteria Kendaraan"
+        ' 
+        ' RbBebek
+        ' 
+        RbBebek.AutoSize = True
+        RbBebek.Location = New Point(69, 22)
+        RbBebek.Name = "RbBebek"
+        RbBebek.Size = New Size(57, 19)
+        RbBebek.TabIndex = 27
+        RbBebek.Text = "Bebek"
+        RbBebek.UseVisualStyleBackColor = True
+        ' 
+        ' Rbsport
+        ' 
+        Rbsport.AutoSize = True
+        Rbsport.Checked = True
+        Rbsport.Location = New Point(9, 22)
+        Rbsport.Name = "Rbsport"
+        Rbsport.Size = New Size(54, 19)
+        Rbsport.TabIndex = 26
+        Rbsport.TabStop = True
+        Rbsport.Text = "Sport"
+        Rbsport.UseVisualStyleBackColor = True
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = SystemColors.ButtonShadow
+        PictureBox3.Location = New Point(12, 331)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(560, 2)
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' btBaru
+        ' 
+        btBaru.Location = New Point(12, 339)
+        btBaru.Name = "btBaru"
+        btBaru.Size = New Size(75, 23)
+        btBaru.TabIndex = 34
+        btBaru.Text = "Baru"
+        btBaru.UseVisualStyleBackColor = True
+        ' 
+        ' btKeluar
+        ' 
+        btKeluar.Location = New Point(93, 339)
+        btKeluar.Name = "btKeluar"
+        btKeluar.Size = New Size(75, 23)
+        btKeluar.TabIndex = 34
+        btKeluar.Text = "Keluar"
+        btKeluar.UseVisualStyleBackColor = True
+        ' 
+        ' Dttanggal
+        ' 
+        Dttanggal.Location = New Point(111, 192)
+        Dttanggal.Name = "Dttanggal"
+        Dttanggal.Size = New Size(264, 23)
+        Dttanggal.TabIndex = 25
+        ' 
+        ' Lbljudul3
+        ' 
+        Lbljudul3.AutoSize = True
+        Lbljudul3.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Lbljudul3.Location = New Point(12, 69)
+        Lbljudul3.Name = "Lbljudul3"
+        Lbljudul3.Size = New Size(214, 15)
+        Lbljudul3.TabIndex = 1
+        Lbljudul3.Text = "Tlp (022)-5417685, Fax (022)-5417686"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(584, 376)
+        Controls.Add(Dttanggal)
+        Controls.Add(btKeluar)
+        Controls.Add(btBaru)
+        Controls.Add(Cbjenis)
+        Controls.Add(Txttotalharga)
+        Controls.Add(Txtpajak)
+        Controls.Add(Txtdiscount)
+        Controls.Add(Txtsubharga)
+        Controls.Add(Txtharga)
+        Controls.Add(Txtalamat)
+        Controls.Add(Txtnama)
+        Controls.Add(Txttransaksi)
+        Controls.Add(GBkriteria)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label12)
+        Controls.Add(Label8)
+        Controls.Add(Label5)
+        Controls.Add(Label9)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox1)
+        Controls.Add(Lbljudul3)
+        Controls.Add(Lbljudul2)
+        Controls.Add(Lbljudul1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Program Penjualan Barang"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        GBkriteria.ResumeLayout(False)
+        GBkriteria.PerformLayout()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Lbljudul1 As Label
+    Friend WithEvents Lbljudul2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Cbjenis As ComboBox
+    Friend WithEvents Txttotalharga As TextBox
+    Friend WithEvents Txtpajak As TextBox
+    Friend WithEvents Txtdiscount As TextBox
+    Friend WithEvents Txtsubharga As TextBox
+    Friend WithEvents Txtharga As TextBox
+    Friend WithEvents Txtalamat As TextBox
+    Friend WithEvents Txtnama As TextBox
+    Friend WithEvents Txttransaksi As TextBox
+    Friend WithEvents GBkriteria As GroupBox
+    Friend WithEvents RbBebek As RadioButton
+    Friend WithEvents Rbsport As RadioButton
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents btBaru As Button
+    Friend WithEvents btKeluar As Button
+    Friend WithEvents Dttanggal As DateTimePicker
+    Friend WithEvents Lbljudul3 As Label
+End Class
---
+++ Program Penjualan Barang/Form1.vb
@@ -0,0 +1,59 @@
+﻿Class Form1
+    Private Sub KodeLostFocusListener() Handles Txttransaksi.LostFocus
+        If Txttransaksi.Text.Trim() Is String.Empty Then
+            MsgBox("Kode Tidak Boleh Kosong", Title:="Error")
+        End If
+    End Sub
+
+    Private Sub KriteriaChangedListener(sender As RadioButton, e As EventArgs) Handles RbBebek.CheckedChanged, Rbsport.CheckedChanged
+        If sender.Checked Then
+            Cbjenis.ResetText()
+            Cbjenis.Items.Clear()
+            If sender Is Rbsport Then
+                Cbjenis.Items.Add("MegaPro")
+                Cbjenis.Items.Add("Tiger")
+            Else
+                Cbjenis.Items.Add("Supra Fit")
+                Cbjenis.Items.Add("Supra X")
+            End If
+        End If
+    End Sub
+
+    Private Sub JenisChangedListener() Handles Cbjenis.TextChanged
+        If Cbjenis.Text.Trim() Is String.Empty Then
+            Txtharga.ResetText()
+            Txtdiscount.ResetText()
+            Txtsubharga.ResetText()
+            Txtpajak.ResetText()
+            Txttotalharga.ResetText()
+        Else
+            Dim harga, discount, subtotal, pajak, total As Double
+
+            harga = IIf(Cbjenis.Text.Trim() = "MegaPro", 26, IIf(Cbjenis.Text.Trim() = "Tiger", 24, IIf(Cbjenis.Text.Trim() = "Supra Fit", 10, 15))) * 1000000
+            discount = harga * 0.05
+            subtotal = harga - discount
+            pajak = subtotal * 0.1
+            total = subtotal + pajak
+
+            Txtharga.Text = FormatCurrency(harga, 0)
+            Txtdiscount.Text = FormatCurrency(discount, 0)
+            Txtsubharga.Text = FormatCurrency(subtotal, 0)
+            Txtpajak.Text = FormatCurrency(pajak, 0)
+            Txttotalharga.Text = FormatCurrency(total, 0)
+        End If
+    End Sub
+
+    Private Sub BaruClickListener(sender As Object, e As EventArgs) Handles btBaru.Click
+        Txttransaksi.ResetText()
+        Txtnama.ResetText()
+        Txtalamat.ResetText()
+        Dttanggal.Value = Date.Now()
+        Rbsport.Select()
+        Cbjenis.ResetText()
+        Txttransaksi.Focus()
+    End Sub
+
+    Private Sub KeluarClickListener(sender As Object, e As EventArgs) Handles btKeluar.Click
+        Close()
+    End Sub
+End Class
